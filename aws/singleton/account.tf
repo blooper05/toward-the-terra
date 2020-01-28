fb168ca9af5b30c7a42bc8da3716fd5b7cbef5c1
blooper05 <legend.of.blooper@gmail.com>
2020-01-28 12:00:40 +0900

    :+1: Add aws_iam_user resources terraform

--- aws/singleton/account.tf
+++ aws/singleton/account.tf
@@ -1,3 +1,27 @@
 resource "aws_iam_account_alias" "alias" {
   account_alias = "blooper"
 }
+
+resource "aws_iam_user" "terraform" {
+  name = "terraform"
+}
+
+resource "aws_iam_access_key" "terraform" {
+  user    = aws_iam_user.terraform.name
+  pgp_key = filebase64("./files/gpg.public.key")
+}
+
+resource "aws_iam_user_policy_attachment" "terraform" {
+  user       = aws_iam_user.terraform.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "aws_iam_access_key-terraform-id" {
+  value     = aws_iam_access_key.terraform.id
+  sensitive = true
+}
+
+output "aws_iam_access_key-terraform-encrypted_secret" {
+  value     = aws_iam_access_key.terraform.encrypted_secret
+  sensitive = true
+}
